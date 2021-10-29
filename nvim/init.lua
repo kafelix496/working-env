680c233e4224c32707010621789dfbf9e3f65a5c
Jiyeol Lee <ka.felix496@gmail.com>
2021-10-28 22:25:30 -0400

    Try to change init.vim -> init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,4 @@
+vim.cmd[[
 source $HOME/.config/nvim/vim-plug/plugins.vim
 source $HOME/.config/nvim/general/settings.vim
 
@@ -12,7 +13,8 @@ if !exists('g:vscode')
   source $HOME/.config/nvim/plug-config/floaterm.vim
   source $HOME/.config/nvim/plug-config/undotree.vim
   source $HOME/.config/nvim/plug-config/airline.vim
-  source $HOME/.config/nvim/plug-config/fzf.vim
+  " source $HOME/.config/nvim/plug-config/fzf.vim
+  source $HOME/.config/nvim/plug-config/telescope.vim
   source $HOME/.config/nvim/plug-config/coc.vim
   source $HOME/.config/nvim/plug-config/far.vim
   source $HOME/.config/nvim/plug-config/jsdoc.vim
@@ -22,3 +24,4 @@ endif
 source $HOME/.config/nvim/plug-config/highlightedyank.vim
 source $HOME/.config/nvim/plug-config/subversive.vim
 source $HOME/.config/nvim/plug-config/quickscope.vim
+]]
