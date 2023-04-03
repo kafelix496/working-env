f015f3e217ff65311f0d6c626d12de6f1d0ef09c
Jiyeol Lee <ka.felix496@gmail.com>
2023-04-03 07:12:18 -0400

    disable auto insertion of comment

--- nvim/lua/custom/options.lua
+++ nvim/lua/custom/options.lua
@@ -32,8 +32,8 @@ local options = {
   wrap = false,                            -- display lines as one long line
 }
 
--- make background transparent
 vim.cmd [[
+  " make background transparent
   highlight Normal guibg=NONE ctermbg=NONE
   highlight NormalNC guibg=NONE ctermbg=NONE
   highlight NvimTreeNormal guibg=NONE ctermbg=NONE
@@ -41,6 +41,9 @@ vim.cmd [[
   highlight NvimTreeNormalNC guibg=NONE ctermbg=NONE
   highlight GitSignsCurrentLineBlame guifg=#ffffff
   highlight PmenuSel guibg=gray ctermbg=gray
+
+  " disable auto insertion of comment
+  autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
 ]]
 
 vim.opt.shortmess:append "c"
