2871b7728cf9b397cb6a372a1c1cd2b4d9259242
Jiyeol Lee <ka.felix496@gmail.com>
2023-06-21 16:15:45 -0400

    add additional keymap

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -1,7 +1,5 @@
 local opts = { noremap = true, silent = true }
 
-local term_opts = { silent = true }
-
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
@@ -57,3 +55,25 @@ keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 -- Visual Block mode end --
 
+vim.cmd [[
+  " nmap <leader>bm <Plug>BookmarkToggle
+  " nmap <leader>bj <Plug>BookmarkNext
+  " nmap <leader>bk <Plug>BookmarkPrev
+
+  " copy current file path to clipboard
+  nnoremap <Leader>cp :let @+ = expand("%")<CR>
+
+  " ripgrep
+  nnoremap <Leader>rg :Rg
+  xnoremap <Leader>rg y:Rg <C-R>"
+
+  " console log
+  nnoremap <Leader>cl :silent put=['console.log(\"<C-r><C-w>\", <C-r><C-w>);']<CR>-2==+
+  xnoremap <Leader>cl y:<C-u>silent put=['console.log(\"<C-r>"\", <C-r>");']<CR>-2==+
+
+  " replace
+  nnoremap <Leader>rp :%s///g<Left><Left>
+  nnoremap <Leader>rc :%s///gc<Left><Left><Left>
+  xnoremap <Leader>rp :%s///g<Left><Left>
+  xnoremap <Leader>rc :%s///gc<Left><Left><Left>
+]]
