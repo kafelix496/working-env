3abe1e148f8b3ec354672cc7fffbcdd34b925732
Jiyeol Lee <ka.felix496@gmail.com>
2023-12-03 17:55:42 -0500

    add lua server and treesitter

--- nvim/lua/custom/treesitter.lua
+++ nvim/lua/custom/treesitter.lua
@@ -1,17 +1,19 @@
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-	return
+  return
 end
 
 configs.setup({
-	ensure_installed = { "typescript", "javascript", "scss", "html", "css", "lua", "vim", "python", "markdown", "go", "graphql", "dockerfile", "json", "regex", "yaml", "http", "prisma" }, -- one of "all" or a list of languages
-	highlight = {
-		enable = true, -- false will disable the whole extension
-	},
-	autopairs = {
-		enable = true,
-	},
-	indent = {
+  -- one of "all" or a list of languages
+  ensure_installed = { "typescript", "javascript", "scss", "html", "css", "lua", "vim", "python", "markdown", "go", "graphql", "dockerfile", "json", "regex", "yaml", "http", "prisma", "lua" },
+  highlight = {
+    -- false will disable the whole extension
+    enable = true,
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = {
     enable = true,
   },
 })
