8b183cdb129afb4e23cd4be38cbb68bc937622dd
Jiyeol Lee <ka.felix496@gmail.com>
2023-12-03 18:11:48 -0500

    disable treesitter highlighting because of error

--- nvim/lua/custom/treesitter.lua
+++ nvim/lua/custom/treesitter.lua
@@ -7,8 +7,9 @@ configs.setup({
   -- one of "all" or a list of languages
   ensure_installed = { "typescript", "javascript", "scss", "html", "css", "lua", "vim", "python", "markdown", "go", "graphql", "dockerfile", "json", "regex", "yaml", "http", "prisma", "lua" },
   highlight = {
+    -- disable because of an error. don't know why.
     -- false will disable the whole extension
-    enable = true,
+    enable = false,
   },
   autopairs = {
     enable = true,
