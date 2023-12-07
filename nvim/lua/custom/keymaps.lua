80ed4f12296b2c94bd7eb9019e35125a66eb1e35
Jiyeol Lee <ka.felix496@gmail.com>
2023-12-07 08:41:33 -0500

    remove unused key map and map tab move

--- nvim/lua/custom/keymaps.lua
+++ nvim/lua/custom/keymaps.lua
@@ -33,15 +33,10 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
--- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
--- Normal mode end --
-
--- Insert mode start --
--- Press jk fast to exit insert mode
-keymap("i", "jk", "<ESC>", opts)
--- Insert mode end --
+-- Navigate tab start --
+keymap("n", "]", ":+tabmove<CR>", opts)
+keymap("n", "[", ":-tabmove<CR>", opts)
+-- Navigate tab end --
 
 -- Visual mode start --
 -- Stay in indent mode
