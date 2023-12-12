767b5f1f1a4476e3a29cdf908b271ae2d331298e
Jiyeol Lee <ka.felix496@gmail.com>
2023-12-11 21:38:26 -0500

    trim code

--- nvim/lua/custom/lsp/handlers.lua
+++ nvim/lua/custom/lsp/handlers.lua
@@ -69,9 +69,9 @@ M.on_attach = function(client, bufnr)
   lsp_keymaps(bufnr)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+vim.lsp.protocol.make_client_capabilities()
 
-local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+local status_ok, _ = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
   return
 end
